8d1da0aaab212e1569f6667c020835e202483398
jenkins <dario.karrer@gmail.com>
2011-11-20 09:39:13 +0100

    Added java-project

---
+++ test/core/tests/PersonTest.java
@@ -0,0 +1,11 @@
+package core.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonTest {
+	@Test
+	public void testIt() {
+		Assert.assertNotNull(Integer.valueOf(23));
+	}
+}
